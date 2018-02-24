2cba59f7c70da7c53b43d0753b7a3c79e288a3ff
Ian Vernon <ian@cilium.io>
2018-02-23 16:10:39 -0800

    tests: migrate 00-check-examples to Ginkgo
    Signed-off by: Ian Vernon <ian@cilium.io>

--- test/runtime/Policies.go
+++ test/runtime/Policies.go
@@ -19,6 +19,8 @@ import (
 	"math/rand"
 	"net"
 	"os"
+	"path/filepath"
+	"sort"
 	"strings"
 	"sync"
 
@@ -28,7 +30,6 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/types"
 	"github.com/sirupsen/logrus"
-	"sort"
 )
 
 const (
@@ -1300,5 +1301,103 @@ var _ = Describe("RuntimeValidatedPolicyImportTests", func() {
 		By("Checking that policy trace returns allowed verdict without any policies imported")
 		res = vm.Exec(fmt.Sprintf(`cilium policy trace --src-endpoint %s --dst-endpoint %s`, httpd2EndpointID, httpd1EndpointID))
 		Expect(res.Output().String()).Should(ContainSubstring(allowedVerdict), "Policy trace did not contain %s", allowedVerdict)
+	})
+})
+
+var _ = Describe("RuntimeValidatedPolicyValidationTests", func() {
+	var once sync.Once
+	var logger *logrus.Entry
+	var vm *helpers.SSHMeta
+
+	initialize := func() {
+		logger = log.WithFields(logrus.Fields{"test": "RuntimeValidatedPolicyValidationTests"})
+		logger.Info("Starting")
+		vm = helpers.CreateNewRuntimeHelper(helpers.Runtime, logger)
+	}
+
+	BeforeEach(func() {
+		once.Do(initialize)
+
+	})
+
+	AfterEach(func() {
+		if CurrentGinkgoTestDescription().Failed {
+			vm.ReportFailed()
+		}
+	})
+
+	It("Validates Example Policies", func() {
+		By("Validating Demos")
+
+		// Helper function which returns the path to all files in directory dir
+		// and all of dir's subdirectories with suffix extension. The file paths
+		// returned contain the path without the prefix dir. This allows for
+		// gathering of the list of files on the host and for the validation
+		// of the policy files to occur the VM, as the root directory of Cilium
+		// is different in each environment.
+		getFilesWithExtensionFromDir := func(dir, extension string) ([]string, error) {
+			fileNames := []string{}
+
+			walkFunc := func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				if strings.HasSuffix(info.Name(), extension) {
+					relativePath := strings.TrimPrefix(path, dir)
+					fileNames = append(fileNames, relativePath)
+				}
+				return nil
+			}
+
+			err := filepath.Walk(dir, walkFunc)
+			if err != nil {
+				return nil, err
+			}
+
+			filesWithExtension := []string{}
+			for _, file := range fileNames {
+				if strings.HasSuffix(file, extension) {
+					filesWithExtension = append(filesWithExtension, file)
+				}
+			}
+			return filesWithExtension, nil
+		}
+
+		examplesDemoPath := "examples/demo"
+		examplesPoliciesPath := "examples/policies"
+		examplePathHost := filepath.Join("..", examplesDemoPath)
+		jsonFiles, err := getFilesWithExtensionFromDir(examplePathHost, "json")
+		Expect(err).Should(BeNil(), "Unable to get files at path %s: %s", examplePathHost, err)
+
+		examplePathVM := filepath.Join(helpers.BasePath, "..", examplesDemoPath)
+		for _, file := range jsonFiles {
+			jsonPolicyPath := filepath.Join(examplePathVM, file)
+			vm.ExecCilium(fmt.Sprintf("policy validate %s", jsonPolicyPath)).ExpectSuccess("Unable to validate policy %s", jsonPolicyPath)
+		}
+
+		By("Validating JSON Examples")
+
+		jsonExamplesPathHost := filepath.Join("..", examplesPoliciesPath)
+		jsonFiles, err = getFilesWithExtensionFromDir(jsonExamplesPathHost, "json")
+		Expect(err).Should(BeNil(), "Unable to get files at path %s: %s", jsonExamplesPathHost, err)
+
+		jsonExamplesPathVM := filepath.Join(helpers.BasePath, "..", examplesPoliciesPath)
+		for _, file := range jsonFiles {
+			jsonPolicyPath := filepath.Join(jsonExamplesPathVM, file)
+			vm.ExecCilium(fmt.Sprintf("policy validate %s", jsonPolicyPath)).ExpectSuccess("Unable to validate policy %s", jsonPolicyPath)
+		}
+
+		By("Validating YAML Examples")
+
+		yamlExamplesPathHost := filepath.Join("..", examplesPoliciesPath)
+		jsonFiles, err = getFilesWithExtensionFromDir(yamlExamplesPathHost, "yaml")
+		Expect(err).Should(BeNil(), "Unable to get files at path %s: %s", yamlExamplesPathHost, err)
+
+		yamlExamplesPathVM := filepath.Join(helpers.BasePath, "..", examplesPoliciesPath)
+		for _, file := range jsonFiles {
+			yamlPolicyPath := filepath.Join(yamlExamplesPathVM, file)
+			res := vm.Exec(fmt.Sprintf("yamllint -c %s %s", filepath.Join(helpers.BasePath, "yaml.config"), yamlPolicyPath))
+			res.ExpectSuccess("Unable to validate YAML %s: %s", yamlPolicyPath, res.CombineOutput())
+		}
 	})
 })
